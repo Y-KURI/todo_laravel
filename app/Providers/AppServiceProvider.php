0d54870e6b3554b56fe792ef8e867a6ede2fb73c
yasu <yasuaki6@gmail.com>
2019-06-25 01:54:46 +0900

    fix provider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env') === 'production' || config('app.env') === 'staging') {
-            \URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
     }
 }
